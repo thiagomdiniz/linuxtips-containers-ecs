945c825d55618b1751896dcc997ab092038aa9f5
Thiago Murilo Diniz <thiagodiniz.info@gmail.com>
2024-12-28 00:21:49 -0500

    adds cloudmap service discovery

--- parameter_store.tf
+++ parameter_store.tf
@@ -21,3 +21,9 @@ resource "aws_ssm_parameter" "lb_internal_listener" {
   type  = "String"
   value = aws_lb_listener.internal.arn
 }
+
+resource "aws_ssm_parameter" "cloudmap" {
+  name  = "/linuxtips/ecs/cloudmap/namespace"
+  type  = "String"
+  value = aws_service_discovery_private_dns_namespace.main.id
+}
